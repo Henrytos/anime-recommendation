57f6dedac0851c4c0710138b84d4cc5e978efd70
Henry <henry.arcaya@sptech.school>
2025-05-02 00:22:06 -0300

    tela home parte 1

---
+++ src/database/script.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE anime;
+
+USE anime;
+DROP TABLE users;
+CREATE TABLE users(
+	userId INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL, 
+    email VARCHAR(45) NOT NULL UNIQUE,
+    password_hash VARCHAR(45) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT chkEmail CHECK(email LIKE '%@%')
+) AUTO_INCREMENT = 1000;
+
+
+INSERT INTO users(username, email, password_hash) VALUES
+	('henry', 'henry@gmail.com', '123456'),
+    ('diego', 'diego@gmail.com', '123456'), 
+    ('maiky', 'maiky@gmail.com', '123456');
+    
+SELECT * FROM users;
+
+UPDATE users SET username = 'maiki' WHERE userid = 3;
+
+DELETE FROM users WHERE userid = 1;
