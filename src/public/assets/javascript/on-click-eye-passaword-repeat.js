fb30f08c9fd6c190cce7c49ae3316c3dd463554e
Henry <henry.arcaya@sptech.school>
2025-05-04 17:32:33 -0300

    feat: funcionalidade de visualização de senha n cadastro e login

--- src/public/assets/javascript/on-click-eye-passaword-repeat.js
+++ src/public/assets/javascript/on-click-eye-passaword-repeat.js
@@ -1,17 +1,19 @@
-const buttonEye = document.getElementById("span_eye")
+const buttonEyeRepeat = document.getElementById("span_eye_repeat")
 
 const svgEyeOpen = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" /><circle cx="12" cy="12" r="3" /></svg>'
 const svgEyeClosed = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-closed-icon lucide-eye-closed"><path d="m15 18-.722-3.25"/><path d="M2 8a10.645 10.645 0 0 0 20 0"/><path d="m20 15-1.726-2.05"/><path d="m4 15 1.726-2.05"/><path d="m9 18 .722-3.25"/></svg>'
 
-let countClick = 0
-buttonEye.addEventListener("click", () => {
-    if (countClick % 2 == 0) {
-        input_password.type = "text"
+let countClickRepeat = 0
+buttonEyeRepeat.addEventListener("click", () => {
+    countClickRepeat
+
+    if (countClickRepeat % 2 == 0) {
+        input_password_repeat.type = "text"
         buttonEye.innerHTML = svgEyeOpen
     } else {
-        input_password.type = "password"
+        input_password_repeat.type = "password"
         buttonEye.innerHTML = svgEyeClosed
     }
 
-    countClick++
+    countClickRepeat++
 })
