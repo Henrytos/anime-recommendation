fbbfea603429b33f1be9683718a9f8fbe7f7b44c
Henry <henry.arcaya@sptech.school>
2025-05-11 00:14:52 -0300

    relacionando tabelas

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE anime_recommendation;
 USE anime_recommendation;
+DROP DATABASE anime_recommendation;
 
 CREATE TABLE users(
-		user_id INT PRIMARY KEY AUTO_INCREMENT,
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(45) NOT NULL, 
     email VARCHAR(45) NOT NULL UNIQUE,
     password_hash VARCHAR(45) NOT NULL,
@@ -10,18 +11,18 @@ CREATE TABLE users(
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT chkEmail CHECK(email LIKE '%@%')
-)AUTO_INCREMENT = 1;
+);
 
 CREATE TABLE quizzes(
-	quiz_id INT PRIMARY KEY AUTO_INCREMENT,
+  quiz_id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(45) NOT NULL,
   description VARCHAR(255) NOT NULL,
-  created_at DATE DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   is_mapping BOOLEAN
 )AUTO_INCREMENT = 1000;
 
 CREATE TABLE mappings(
-	mapping_id INT PRIMARY KEY AUTO_INCREMENT, 
+  mapping_id INT PRIMARY KEY AUTO_INCREMENT, 
   target_audience VARCHAR(45) NOT NULL,
   gender VARCHAR(45) NOT NULL,
   studio VARCHAR(45) NOT NULL,
@@ -31,58 +32,70 @@ CREATE TABLE mappings(
   CONSTRAINT chk_gender CHECK(gender IN ('ação', 'aventura', 'romance', 'comédia', 'slice of Life', 'drama')),
   CONSTRAINT chk_studio CHECK(studio IN ('mappa', 'ghibli', 'ufotable', 'toei')),
   CONSTRAINT chk_type_duration CHECK(type_duration IN ('curto', 'médio', 'longo')),
-  CONSTRAINT chk_type_style CHECK(type_style IN ('realista', 'cartunesco', '3d', 'nostálgico')),
+  CONSTRAINT chk_type_style CHECK(type_style IN ('realista', 'cartunesco', '3d', 'nostálgico'))
 )AUTO_INCREMENT=2000;
 
-CREATE TABLE anime(
-	anime_id INT,
+CREATE TABLE animes(
+  anime_id INT,
   api_anime_id INT, 
   fk_mapping_id INT,
-  name VARCHAR(45),
+  title VARCHAR(45),
   image_url VARCHAR(255),
   description VARCHAR(255),
-  CONSTRAINT pk_composite PRIMARY KEY(anime_id, api_anime_id, fk_mapping_id)
+  CONSTRAINT pk_composite PRIMARY KEY(anime_id, api_anime_id, fk_mapping_id),
+  CONSTRAINT fk_anime_mapping FOREIGN KEY (fk_mapping_id) REFERENCES mappings(mapping_id)
 );
 
 CREATE TABLE comments(
 	comment_id INT,
   fk_anime_id INT,
   fk_user_id INT, 
-  created_at DATE DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   description VARCHAR(255) NOT NULL,
-  CONSTRAINT pk_composite PRIMARY KEY(comment_id, fk_anime_id, fk_user_id)
+  CONSTRAINT pk_composite PRIMARY KEY(comment_id, fk_anime_id, fk_user_id),
+  CONSTRAINT fk_comment_anime FOREIGN KEY (fk_anime_id) REFERENCES animes (anime_id),
+  CONSTRAINT fk_comment_user FOREIGN KEY (fk_user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE questions(
-	question_id INT AUTO_INCREMENT,
+  question_id INT AUTO_INCREMENT,
   fk_quiz_id INT,
-	tilte VARCHAR(45) NOT NULL,
+  tilte VARCHAR(45) NOT NULL,
   number INT NOT NULL,
-  CONSTRAINT pk_composite PRIMARY KEY(question_id, fk_quiz_id)
+  CONSTRAINT pk_composite PRIMARY KEY(question_id, fk_quiz_id),
+  CONSTRAINT fk_question_quiz FOREIGN KEY (question_id) REFERENCES quizzes(quiz_id)
 );
 
 CREATE TABLE alternatives(
-	alternative_id INT,
+  alternative_id INT,
   fk_mapping_id INT,
   title VARCHAR(45),
   description VARCHAR(255),
   image_url VARCHAR(45),
   is_correct BOOLEAN,
-  CONSTRAINT pk_composite PRIMARY KEY(alternative_id, fk_mapping_id)
+  CONSTRAINT pk_composite PRIMARY KEY(alternative_id, fk_mapping_id),
+  CONSTRAINT fk_alternative_mapping FOREIGN KEY (fk_mapping_id) REFERENCES mappings(mapping_id)
 );
 
 CREATE TABLE options(
   fk_alternative_id INT, 
   fk_question_id INT,
   fk_quiz_id INT,
-  CONSTRAINT pk_composite PRIMARY KEY(fk_alternative_id, fk_question_id, fk_quiz_id)
+  CONSTRAINT pk_composite PRIMARY KEY(fk_alternative_id, fk_question_id, fk_quiz_id),
+  CONSTRAINT fk_option_alternative FOREIGN KEY (fk_alternative_id) REFERENCES alternatives (alternative_id),
+  CONSTRAINT fk_option_question FOREIGN KEY (fk_question_id) REFERENCES questions (question_id),
+  CONSTRAINT fk_option_quiz FOREIGN KEY (fk_quiz_id) REFERENCES quizzes (quiz_id)
 );
 
 
 CREATE TABLE quiz_result(
-	fk_user INT,
+  fk_user_id INT,
   fk_quiz_id INT,
   fk_anime_id INT,
   fk_mapping_id INT,
-  CONSTRAINT pk_composite PRIMARY KEY(fk_user, fk_quiz_id, fk_anime_id, fk_mapping_id)
+  CONSTRAINT pk_composite PRIMARY KEY(fk_user_id, fk_quiz_id, fk_anime_id, fk_mapping_id),
+  CONSTRAINT quiz_result_user FOREIGN KEY (fk_user_id) REFERENCES users (user_id),
+  CONSTRAINT quiz_result_quiz FOREIGN KEY (fk_quiz_id) REFERENCES quizzes (quiz_id),
+  CONSTRAINT quiz_result_anime FOREIGN KEY (fk_anime_id) REFERENCES animes (anime_id),
+  CONSTRAINT quiz_result_mapping FOREIGN KEY (fk_mapping_id) REFERENCES mappings (mapping_id)
 );
