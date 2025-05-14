48f6ecc2832f84656cf0f8209aa1db17c224dd70
Henry <henry.arcaya@sptech.school>
2025-05-14 09:44:40 -0300

    feat: animação na apresentação do quiz

--- public/assets/javascript/on-load-render-quiz.js
+++ public/assets/javascript/on-load-render-quiz.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", () => {
   section_container.style.backgroundImage = "url('')";
+
+  const title = document.getElementById("title-quiz");
+  title.classList.remove("inactive-title");
+  title.classList.add("active-title");
 });
