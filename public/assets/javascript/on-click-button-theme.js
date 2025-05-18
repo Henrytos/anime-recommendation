da36ea496a6d8819f6e4376dced383dc03a4ef97
Henry <henry.arcaya@sptech.school>
2025-05-18 15:30:17 -0300

    feat: funcionalidade de alterar de tema

--- public/assets/javascript/on-click-button-theme.js
+++ public/assets/javascript/on-click-button-theme.js
@@ -1,14 +1,56 @@
+const colors = {
+  dark: {
+    "--color-white": "#f3f3f3",
+    "--color-background": "#141617",
+    "--color-background-foreground": "#f3f3f3",
+    "--color-purple-light": "#a39fbf",
+    "--color-purple-black": "#313646",
+    "--color-red": "#cb4648",
+    "--color-red-light": "#d97f77",
+  },
+  light: {
+    "--color-white": "#141617",
+    "--color-background": "#C6C6C7",
+    "--color-background-foreground": "#141617",
+    "--color-purple-light": "#313646",
+    "--color-purple-black": "#313646",
+    "--color-red": "#cb4648",
+    "--color-red-light": "#d97f77",
+  },
+};
 
-const buttonTheme = document.getElementById("button_theme")
+const background = {
+  "/index.html": {
+    selector: ".start",
+    dark: {
+      file: "background-start.png",
+    },
+    light: {
+      file: "background-start-white.png",
+    },
+  },
+};
+
+const buttonTheme = document.getElementById("button_theme");
 
 buttonTheme.addEventListener("click", () => {
-    let body = document.querySelector("body")
-    let newTheme = body.className == "dark" ? "light" : "dark"
+  let body = document.querySelector("body");
+  let theme = body.className == "dark" ? "light" : "dark";
 
-    body.className = newTheme
-    toggleThemeInApplication(newTheme)
-})
+  body.className = theme;
+  const pallet = colors[theme];
 
-function toggleThemeInApplication(theme) {
-    console.log(theme)
-}
+  for (let key in pallet) {
+    let value = pallet[key];
+    body.style.setProperty(key, value);
+  }
+
+  const path = window.location.pathname;
+  const elementBackground = document.querySelector(background[path].selector);
+  const filename = background[path][theme].file;
+  elementBackground.style.backgroundImage = `url("./assets/images/${filename}")`;
+});
+
+// quizzes.html #section_container;
+// index.html .start
+// quiz.html .background
