c86bef5529120536525e7b813553afb98aa60652
Henry <henry.arcaya@sptech.school>
2025-05-24 12:13:44 -0300

    feat: controller de busca de recomendações do usuario

---
+++ src/controllers/fetch-recommendations.controller.js
@@ -0,0 +1,33 @@
+const userModel = require("../models/user.model.js");
+const recommendationsModel = require("../models/recommendations.model.js");
+
+function fetchRecommendationsController(request, response) {
+  const userId = request.headers.userId;
+
+  const isBadRequestData = userId == undefined;
+  if (isBadRequestData) {
+    return response.status(400).json({
+      message: "invalid data",
+    });
+  }
+
+  userModel.findByUserId(userId).then((users) => {
+    const isNotExistsUser = users.length == 0;
+
+    if (isNotExistsUser) {
+      return response.status(401).json({
+        message: "unauthorized user",
+      });
+    }
+  });
+
+  recommendationsModel
+    .findManyRecommendations(userId)
+    .then((recommendations) => {
+      return response.status(200).json({
+        recommendations,
+      });
+    });
+}
+
+module.exports = { fetchRecommendationsController };
