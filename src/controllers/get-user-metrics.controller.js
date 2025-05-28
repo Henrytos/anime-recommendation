739e59c2d0f5ea00b77973cf0b1110aacf5ad355
Henry <henry.arcaya@sptech.school>
2025-05-27 23:48:19 -0300

    feat:controller de metricas do usuario

---
+++ src/controllers/get-user-metrics.controller.js
@@ -0,0 +1,36 @@
+const userModel = require("../models/users.model.js")
+const recommendationsModel = require("../models/recommendations.model.js")
+
+
+async function getUserMetricsController(request, response) {
+    const userId = request.headers.userid;
+
+    const isBadRequestData = userId == undefined;
+    if (isBadRequestData) {
+        return response.status(400).json({
+            message: "invalid data",
+        });
+    }
+
+    const users = await userModel.findByUserId(userId)
+    const usersNotFound = users.length === 0
+
+    if (usersNotFound) {
+        return response.status(401).json({
+            message: "unauthorized user",
+        });
+    }
+
+    const userMetricsQuizzes = await recommendationsModel.findByUserMetricsQuizzes(userId)
+    const userMetricsMappings = await recommendationsModel.findByUserMetricsMappings(userId)
+
+    return response.status(200).json({
+        quizzes: userMetricsQuizzes,
+        mappings: userMetricsMappings
+    })
+}
+
+
+module.exports = {
+    getUserMetricsController,
+};
