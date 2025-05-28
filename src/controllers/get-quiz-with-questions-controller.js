7a0d65506802d5782d915bfc261469a53f996676
Henry <henry.arcaya@sptech.school>
2025-05-28 01:00:40 -0300

    feat: controller de retorno de alternativas de uma questão

--- src/controllers/get-quiz-with-questions-controller.js
+++ src/controllers/get-quiz-with-questions-controller.js
@@ -1,10 +1,59 @@
 const view = require("../models/view.model");
 
-function getQuizWithQuestionsController(request, response) {
+async function getQuizWithQuestionsController(request, response) {
   const quizId = request.params.quizId;
 
-  view.getQuizWithQuestionsAlternative(quizId).then((result) => {
-    return response.status(200).json();
+  const quizWithQuestionsAlternative = await view.getQuizWithQuestionsAlternative(quizId)
+
+
+  const questionsWithAlternatives = []
+
+  let quantityQuestions = []
+
+  for (let row = 0; row < quizWithQuestionsAlternative.length; row++) {
+    let result = quizWithQuestionsAlternative[row]
+
+    if (!quantityQuestions.includes(result.question_id)) {
+      quantityQuestions.push(result.question_id)
+    }
+  }
+
+
+  for (let row = 0; row < quantityQuestions.length; row++) {
+    let questionId = quantityQuestions[row]
+    let alternatives = []
+
+    for (let rowAlternative = 0; rowAlternative < quizWithQuestionsAlternative.length; rowAlternative++) {
+      let result = quizWithQuestionsAlternative[rowAlternative]
+
+      let alternative = {
+        id: result.alternative_id,
+        question_id: result.alternative_question_id,
+        quiz_id: result.alternative_quiz_id,
+        title: result.alternative_title,
+        description: result.alternative_description,
+        target_audience: result.alternative_target_audience,
+        gender: result.alternative_gender,
+        image_url: result.alternative_image_url,
+      }
+
+      if (questionId == alternative.question_id) {
+        alternatives.push(alternative)
+      }
+
+
+    }
+
+    questionsWithAlternatives.push({
+      // id: result.question_id,
+      // title: result.quiz_title,
+      alternatives
+    })
+
+  }
+
+  return response.status(200).json({
+    questions: questionsWithAlternatives,
   });
 }
 
--- src/models/view.model.js
+++ src/models/view.model.js
@@ -1,7 +1,27 @@
 const database = require("../database/config");
 
-function getQuizWithQuestionsAlternative(quizId) {
-  const query = ` SELECT * FROM quiz_with_questions WHERE quiz_id = ${quizId}`;
+function getQuizWithQuestionsAlternative() {
+  const query = `
+ SELECT
+	quizzes.quiz_id AS 'quiz_id',
+  quizzes.title AS 'quiz_title',
+  questions.question_id AS 'question_id',
+  questions.title AS 'question_title',
+	questions.number AS 'question_number',
+  alternatives.alternative_id AS 'alternative_id',
+  alternatives.fk_question_id AS 'alternative_question_id',
+	alternatives.fk_quiz_id AS 'alternative_quiz_id',
+	alternatives.title AS 'alternative_title',
+  alternatives.description AS 'alternative_description',
+  alternatives.target_audience AS 'alternative_target_audience',
+  alternatives.gender AS 'alternative_gender',
+  alternatives.image_url AS 'alternative_image_url'
+FROM quizzes
+	JOIN questions ON quizzes.quiz_id = questions.fk_quiz_id
+  JOIN alternatives ON alternatives.fk_question_id = questions.question_id AND alternatives.fk_quiz_id = quizzes.quiz_id
+  WHERE quizzes.quiz_id = 1000
+  ORDER BY number;
+  `;
 
   return database.execute(query);
 }
