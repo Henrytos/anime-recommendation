8eb6fd00f81646a491d3b8a129b8047625e58e0d
Henry <henry.arcaya@sptech.school>
2025-05-28 21:08:51 -0300

    fix: pequeno bug de referencia

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.listen(PORT_APP, function () {
     ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
     \n\n\n                                                                                                 
     Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORT_APP} :. \n\n
-    Você está rodando sua aplicação em ambiente de .:${process.env.enviroment}:. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
