abff611217d57d9d87413cc8d4a7f082b23010bd
Henry <henry.arcaya@sptech.school>
2025-05-31 23:16:42 -0300

    feat: renderizando quiz (on-load-render-quiz.js)

--- public/assets/javascript/on-load-render-quiz.js
+++ public/assets/javascript/on-load-render-quiz.js
@@ -6,170 +6,91 @@ window.addEventListener("load", () => {
   fetchQuizById(quizId);
 });
 
-function fetchQuizById(quizId) {
-  // fetch(`http://localhost:3333/quiz/${quizId}`)
-  //   .then((response) => response)
-  //   .then((data) => data.json())
-  //   .then((quiz) => {
-  //     console.log(quiz);
-
-  //   });
+async function fetchQuizById(quizId) {
   const title = document.getElementById("title-quiz");
-  title.innerHTML = "QUAL ANIME COMBINA COM VOCÊ?";
+
+  const response = await fetch(`/quiz/${quizId}`)
+  const data = await response.json()
+
+  title.innerHTML = data.title;
   title.classList.remove("slide-new");
   title.classList.add("slide-show");
 
-  // const questions = quiz.questions;
+  const questions = data.questions;
 
-  renderQuestions();
+  renderQuestions(questions);
 }
 
-function renderQuestions() {
+function renderQuestions(questions) {
   const contentQuestions = document.getElementById("div_questions");
 
-  // questions.map((question) => {
-  //   console.log(question);
+  let contentQuestionsHTML = ''
 
-  // });
+  for (let positionQuestion = 0; positionQuestion < questions.length; positionQuestion++) {
+    let question = questions[positionQuestion]
 
-  contentQuestions.innerHTML += `
-  <section class='question question-1 slide-new'>
-  <div class='left'>
-      <h1>1 - Qual estilo de narrativa te agrada mais ?</h1>
 
-      <div class='content-progress'>
+    contentQuestionsHTML += `
+    <section class='question question-1 slide-new'>
+    <div class='left'>
+    <h1>${question.id} - ${question.title}</h1>
+
+    <div class='content-progress'>
         <div class='line-progress'></div>
-      </div>
-    
-      <div class='content-alternatives'>
+        </div>
+
+        <div class='content-alternatives'>
+        
+        `
+
+    for (let positionAlternative = 0; positionAlternative < question.alternatives.length; positionAlternative++) {
+      let alternative = question.alternatives[positionAlternative]
+      contentQuestionsHTML += `
         <div class='alternative' onclick='selectionAlternative(1, 1)'>
           <img src='https://gqcanimes.com.br/wp-content/uploads/2021/07/Konosuba-GQCA1.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
+          <div div class='content-alternative'>
             <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
+            ${alternative.description}
             </p>
-            <h2>
-              ISEKAI
+            <h2>      
+            ${alternative.title}
             </h2>
           </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(2, 1)'>
-          <img src='https://animeflix.com.br/wp-content/uploads/2024/12/My-Hero-Academia-se-atualize-sobre-a-temporada-final-do-anime-696x464.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              SHOUNEN
-            </h2>
-          </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(3, 1)'>
-          <img src='https://m.media-amazon.com/images/S/pv-target-images/286a0e266e2521f56a810653db79e2dfa4de7e9f80286b321085bcf9e75f43fb._SX1080_FMjpg_.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              GORE
-            </h2>
-          </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(4, 1)'>
-          <img src='https://sm.ign.com/ign_br/screenshot/default/haikyu-1_arwc.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              SPORTS
-            </h2>
-          </div>
-        </div>
+      </div>`
+    }
+
+    let characterImageUrl = `character-makima.png`
+
+    if (positionQuestion % 2 == 1) {
+      characterImageUrl = 'character-power.png'
+    }
+
+    contentQuestionsHTML += `
       </div>
-  </div>
-  
-  <div class='right'>
-  <img src='./assets/images/character-makima.png' alt='image url' id='character-makima' />
-  </div>
-  </section>
-
-   <section class='question question-1 slide-new'>
-  <div class='left'>
-      <h1>
-        2 - Qual estilo de animação te mais agrada ?
-      
-      </h1>
-
-      <div class='content-progress'>
-        <div class='line-progress'></div>
       </div>
-    
-      <div class='content-alternatives'>
-        <div class='alternative' onclick='selectionAlternative(1, 1)'>
-          <img src='https://gqcanimes.com.br/wp-content/uploads/2021/07/Konosuba-GQCA1.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              ISEKAI
-            </h2>
-          </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(2, 1)'>
-          <img src='https://animeflix.com.br/wp-content/uploads/2024/12/My-Hero-Academia-se-atualize-sobre-a-temporada-final-do-anime-696x464.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              SHOUNEN
-            </h2>
-          </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(3, 1)'>
-          <img src='https://m.media-amazon.com/images/S/pv-target-images/286a0e266e2521f56a810653db79e2dfa4de7e9f80286b321085bcf9e75f43fb._SX1080_FMjpg_.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              GORE
-            </h2>
-          </div>
-        </div>
-        <div class='alternative' onclick='selectionAlternative(4, 1)'>
-          <img src='https://sm.ign.com/ign_br/screenshot/default/haikyu-1_arwc.jpg' alt='thumb url de konosuba' />
-          <div class='content-alternative'>
-            <p>
-              histórias onde um protagonista é transportado para um mundo diferente, seja por teletransporte, reencarnação ou outros meios.
-            </p>
-            <h2>
-              SPORTS
-            </h2>
-          </div>
-        </div>
       </div>
-  </div>
-  
-  <div class='right'>
-  <img src='./assets/images/character-power.png' alt='image url' id='character-makima' />
-  </div>
-  </section>
+
+      <div class='right'>
+        <img src='./assets/images/${characterImageUrl}' alt='image url' id='image character cover url: ${characterImageUrl}' />
+      </div>
+    </section>
+    `
+  }
+
+  contentQuestionsHTML += `
+    <div class="content-final slide-new">
+        <h1  id="result-final">
+            RESULTADO FINAL:
+        </h1>
 
 
-  <div class="content-final slide-new">
-      <h1  id="result-final">
-          RESULTADO FINAL:
-      </h1>
+        <img id="image-question-1" src="./assets/images/loli-like.png" alt="imagem da loli da duvida">
+        <img id="image-question-2" src="./assets/images/loli-congratulations.png" alt="imagem da loli da duvida 2">
 
+    </div>
+  `
+  contentQuestions.innerHTML = contentQuestionsHTML
 
-      <img id="image-question-1" src="./assets/images/loli-like.png" alt="imagem da loli da duvida">
-      <img id="image-question-2" src="./assets/images/loli-congratulations.png" alt="imagem da loli da duvida 2">
-
-  </div>
-  `;
 
   const questionsSection = document.querySelectorAll(".question");
 
