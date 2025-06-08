03952bd1bd2b14b0f5e7aed2952c607c6d6ac6c4
Henry <henry.arcaya@sptech.school>
2025-06-08 10:58:15 -0300

    fix(controller): arrumando bugs de paginação

--- src/controllers/fetch-recommendations.controller.js
+++ src/controllers/fetch-recommendations.controller.js
@@ -53,44 +53,42 @@ async function fetchRecommendationsController(request, response) {
   const gender = request.query.gender || "all"
   const targetAudience = request.query.targetAudience || "all"
 
-  console.log({
-    gender,
-    targetAudience
-  })
-
   for (let positionRecommendation = 0; positionRecommendation < recommendations.length; positionRecommendation++) {
     const recommendationTarget = recommendations[positionRecommendation]
 
     if (gender !== "all" && targetAudience !== "all" && recommendationTarget.gender == gender && recommendationTarget.target_audience == targetAudience) {
       recommendationsFilter.push(recommendationTarget)
-
     }
 
-    if (gender !== "all" && recommendationTarget.gender == gender) {
+    if (gender !== "all" && recommendationTarget.gender == gender && targetAudience === "all") {
       recommendationsFilter.push(recommendationTarget)
-
     }
 
-    if (targetAudience !== "all" && recommendationTarget.target_audience == targetAudience) {
+    if (targetAudience !== "all" && recommendationTarget.target_audience == targetAudience && gender === "all") {
       recommendationsFilter.push(recommendationTarget)
-
     }
 
     if (gender == "all" && targetAudience == "all") {
       recommendationsFilter.push(recommendationTarget)
     }
-
   }
 
-  const maxPages = Math.floor(recommendationsFilter.length / perPage)
+  const maxPages = Math.ceil(recommendationsFilter.length / perPage)
+
+  if (page > maxPages) {
+    return response.status(400).json({
+      message: "invalid page",
+    });
+  }
+
+  const recommendationFilterByPage = recommendationsFilter.slice((page - 1) * perPage, perPage * page)
 
   return response.status(200).json({
     page,
-    perPage,
     maxPages,
     filtersGender,
     filtersTargetAudience,
-    recommendationsFilter
+    recommendations: recommendationFilterByPage
   });
 }
 
