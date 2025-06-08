41490160ed35b0f4a12ef9798448e5287332af63
Henry <henry.arcaya@sptech.school>
2025-06-08 09:21:09 -0300

    feat: trabalhando com paginaçã com controller de todas as recomendações de animes do usuario

--- src/controllers/fetch-recommendations.controller.js
+++ src/controllers/fetch-recommendations.controller.js
@@ -20,10 +20,77 @@ async function fetchRecommendationsController(request, response) {
     });
   }
 
+
+  const page = Number(request.query.page) || 1
+  const perPage = 10
+
+
+
   const recommendations = await recommendationsModel.findManyRecommendations(userId)
 
+  const filtersGender = []
+
+  for (let position = 0; position < recommendations.length; position++) {
+    const recommendation = recommendations[position]
+
+    if (!filtersGender.includes(recommendation.gender)) {
+      filtersGender.push(recommendation.gender)
+    }
+  }
+
+  const filtersTargetAudience = []
+
+  for (let position = 0; position < recommendations.length; position++) {
+    const recommendation = recommendations[position]
+
+    if (!filtersTargetAudience.includes(recommendation.target_audience)) {
+      filtersTargetAudience.push(recommendation.target_audience)
+    }
+  }
+
+  const recommendationsFilter = []
+
+  const gender = request.query.gender || "all"
+  const targetAudience = request.query.targetAudience || "all"
+
+  console.log({
+    gender,
+    targetAudience
+  })
+
+  for (let positionRecommendation = 0; positionRecommendation < recommendations.length; positionRecommendation++) {
+    const recommendationTarget = recommendations[positionRecommendation]
+
+    if (gender !== "all" && targetAudience !== "all" && recommendationTarget.gender == gender && recommendationTarget.target_audience == targetAudience) {
+      recommendationsFilter.push(recommendationTarget)
+
+    }
+
+    if (gender !== "all" && recommendationTarget.gender == gender) {
+      recommendationsFilter.push(recommendationTarget)
+
+    }
+
+    if (targetAudience !== "all" && recommendationTarget.target_audience == targetAudience) {
+      recommendationsFilter.push(recommendationTarget)
+
+    }
+
+    if (gender == "all" && targetAudience == "all") {
+      recommendationsFilter.push(recommendationTarget)
+    }
+
+  }
+
+  const maxPages = Math.floor(recommendationsFilter.length / perPage)
+
   return response.status(200).json({
-    recommendations,
+    page,
+    perPage,
+    maxPages,
+    filtersGender,
+    filtersTargetAudience,
+    recommendationsFilter
   });
 }
 
